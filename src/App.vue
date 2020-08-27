4641dd2d5e86c682f71531b9258c221da09793bf
oleoot <oleoot28@gmail.com>
2020-08-27 20:33:49 +0300

    Displaying weather data on the page

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,13 @@
           @keypress="fetchWeather"
         />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northhampton, UK</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">Monday 20 January 2020</div>
           <div class="weather-box">
-            <div class="temp">9c</div>
-            <div class="weather">Rain</div>
+            <div class="temp">{{Math.round(weather.main.temp)}}c</div>
+            <div class="weather">{{weather.weather[0].main}}</div>
           </div>
         </div>
       </div>
@@ -39,7 +39,7 @@ export default {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
-          `${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
         )
           .then((res) => {
             return res.json();
