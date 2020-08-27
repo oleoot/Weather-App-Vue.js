49f1ebf43ec93aaeb5df7d06da37b40aa7db3356
oleoot <oleoot28@gmail.com>
2020-08-27 20:42:43 +0300

    New bg images

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefind' && weather.main.temp>16? 'warm': ''">
     <main>
       <div class="search-box">
         <input
